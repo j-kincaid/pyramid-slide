a4d0f567655909d8bc7928ec6a55bc4abfc5774e
Jessica Kincaid <4j.kincaid@gmail.com>
2018-12-14 17:51:21 -0600

    UX improvements

--- pyramid.js
+++ pyramid.js
@@ -1,5 +1,50 @@
+/**
+ * determineHeightAndThenDrawPyramid
+ *
+ * Determines the current value that the user has typed in the 'How high?' text-box,
+ * and then draws a pyramid with that height.
+ */
+function determineHeightAndThenDrawPyramid() {
+
+    // just so we know we're here
+    console.log("someone invoked the determineHeightAndThenDrawPyramid function!");
+
+    // TODO 3
+    // figure out the height the user typed (replace the "5" below)
+
+    var heightInput = document.getElementById("height")
+    heightStr = heightInput.value
+
+
+    // here we convert the string to an int
+    height = parseInt(heightStr);
+
+    // TODO 2
+    // draw the pyramid with the given height
+    drawPyramid(height)
+
+}
+
+
+// TODO 1
+// hook up the button's click event to our determineHeightAndThenDrawPyramid function
+var button = document.getElementsByTagName("button")[0]
+button.addEventListener("click", determineHeightAndThenDrawPyramid)
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
 function drawPyramid(height) {
 
+    // TODO 4
+    // before drawing, clear the old content
+    document.getElementById("pyramid").dinnerHTML = ""
+
+
+    // for each row....
     for (var row = 0; row < height; row++) {
 
         // figure out number of bricks and spaces
@@ -8,13 +53,21 @@ function drawPyramid(height) {
 
         // build up a string for this row
         var rowStr = "";
-        for (var i = 1; i < numSpaces; i++) {
-            rowStr += " "; // QUIZ: what happens if we use a space (" ") instead of a period?
+        for (var i = 0; i < numSpaces; i++) {
+            rowStr += ".";
         }
-        for (var i = 1; i < numBricks; i++) {
+        for (var i = 0; i < numBricks; i++) {
             rowStr += "#";
         }
 
-        p = document.innerHTML("rowStr");
-        pyramidDiv = document.getElementById("pyramid");
+        // create a text element with the string of characters
+        textElem = document.createTextNode(rowStr);
+
+        // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
     }
+}
