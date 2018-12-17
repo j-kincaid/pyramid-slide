14b800a0834245a55f6d8c0c1f79eba432b23241
Jessica Kincaid <4j.kincaid@gmail.com>
2018-12-17 11:43:01 -0600

    added onchange event to brick symbol menu

--- pyramid.js
+++ pyramid.js
@@ -21,11 +21,12 @@ document.getElementById("height").addEventListener("mousemove", function() {
 
 drawPyramid();
 
-// Change characters in bricks menu
-// function pyrChar() {
-//     var x = document.getElementById("brick-symbol").value;
-//     document.getElementById("bricks").innerHTML = "option value= " + x;
-// }
+Change characters in bricks menu
+
+function pyrChar() {
+    var x = document.getElementById("brick-symbol").value;
+    document.getElementById("bricks").innerHTML = "option value= " + x;
+}
 
 // draw pyramid with the specified height
 
